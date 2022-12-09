9e2f3216c8664c24b559588085111fbfb2b16f82
pibberflibbits <pibberflibbits@gmail.com>
2022-12-08 20:23:18 -0800

    first commit

--- guessing-game/target/debug/deps/guessing_game-4605d98e30741863.d
+++ guessing-game/target/debug/deps/guessing_game-4605d98e30741863.d
@@ -1,5 +1,5 @@
-/home/carter/Documents/Rust-Scripts/guessing-game/target/debug/deps/guessing_game-4605d98e30741863: src/main.rs
+/home/carter/Documents/rust-sketches/guessing-game/target/debug/deps/guessing_game-4605d98e30741863: src/main.rs
 
-/home/carter/Documents/Rust-Scripts/guessing-game/target/debug/deps/guessing_game-4605d98e30741863.d: src/main.rs
+/home/carter/Documents/rust-sketches/guessing-game/target/debug/deps/guessing_game-4605d98e30741863.d: src/main.rs
 
 src/main.rs:
