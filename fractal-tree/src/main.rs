cb080064654c37e8eee0c9f4cc99538f658a8ed8
pibberflibbits <pibberflibbits@gmail.com>
2022-12-10 18:28:49 -0800

    first commit

--- fractal-tree/src/main.rs
+++ fractal-tree/src/main.rs
@@ -1,3 +1,56 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dfb7a0a31d35731c33912f8e1cfa8a8bba8abfc86c03737bf4e50b10d5aa0b23
-size 1073
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn create_tree(draw: Draw) {
+    let mut depth = 0.0;
+    let mut angle = 0.0; 
+
+    if depth <= 0.0 {
+        draw.rect()
+            .x_y(0.0,0.0)
+            .w_h(100.0,100.0)
+            .z_degrees(angl)
+            .color(GREEN);
+    }
+
+    if depth != 0.0 {
+
+    draw.rect()
+            .x_y(0.0,0.0)
+            .w_h(100.0,100.0)
+            .z_degrees(newangle)
+            .color(GREEN);
+            depth = depth + 1.0;
+            angle = angle + 90.0;
+    }
+
+}
+
+fn view(app: &App, _model: &Model, frame: Frame){
+    // define drawing variables that will be passed through
+    // to the create_tree function.
+    let draw = app.draw();
+    let draw2 = app.draw();
+
+    // creates the tree using recursion
+    create_tree(draw);
+
+    // Draws tree to frame
+    draw2.to_frame(app, &frame).unwrap();
+}
+
