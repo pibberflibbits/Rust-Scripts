dc17339adfb59447607a44c5cb23600022c67b0e
pibberflibbits <pibberflibbits@gmail.com>
2022-12-13 14:07:14 -0800

    first commit

---
+++ Fractal-Tree/src/main.rs
@@ -0,0 +1,62 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn view(_app: &App, _model: &Model, frame: Frame){
+    let draw2 = _app.draw();
+
+    // Draw Fractal Tree
+    create_tree(_app, pt2(0.0, 0.0), pt2(0.0, 0.0));
+    
+    draw2.to_frame(_app, &frame).unwrap();
+}
+
+fn create_tree(_app: &App, spoint: Vec2, epoint: Vec2) {
+    
+    let mut angle = 0.0;
+    let mut depth = 0.0;
+
+    loop {
+    let draw = _app.draw();
+    let angleinc = PI / 4.0;
+    let increasecalc = 50.0 * 0.2.pow(depth);
+    let increase = increasecalc as f32;
+    let nextspoint = spoint * 0.2;
+    let gaming = nextspoint.y as f32;
+    let gamingx = nextspoint.x as f32;
+    
+    draw.line()
+        .start(spoint)
+        .end(epoint)
+        .weight(1.0)
+        .z_degrees(angle)
+        .color(GREEN);
+        angle = angle + angleinc;
+        depth = depth + 1.0;
+        dbg!(depth);
+        
+        if depth < 15.0 {
+
+            create_tree(_app, epoint, pt2(gamingx, increase))
+        }
+
+        else {
+            break;
+        }
+    }
+}
+
